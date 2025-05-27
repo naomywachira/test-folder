050ee1d1035a0946261b16df4da1f67b05b7e8d8
naomywachira <naomyoketch45@gmail.com>
2025-05-27 14:01:48 +0300

    Add files via upload

---
+++ example.js
@@ -0,0 +1,16 @@
+//console.log(" hellow world"); 
+
+
+/*let person={
+    name:'esther',
+    age:20
+};*/
+//console.log(person);
+ 
+/*let selectedColors=['red','green'];
+console.log(selectedColors[0]);*/
+
+function test(){
+    console.log('fffffffffffffff');
+};
+test();
